797d78ee6a6eb362714e90ac0384f3a141705109
jasonlock123 <jason@locksplace.com>
2017-12-01 05:50:29 +0000

    Update fireworks.ts

---
+++ fireworks.ts
@@ -0,0 +1,8 @@
+const magik = magikcraft.io;
+
+function fireworks() {
+    function firework() {
+        const there = magik.aspecto();
+        magik.stella(there);
+    }
+    magik.doNTimes(firework, 5);
