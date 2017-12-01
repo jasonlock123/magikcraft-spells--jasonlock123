feee1823f0d8771bb15e172ca5d364108c0ea54b
jasonlock123 <jason@locksplace.com>
2017-12-01 05:50:33 +0000

    Delete fireballz.ts

--- fireballz.ts
+++
@@ -1,9 +0,0 @@
-const magik = magikcraft.io;
-
-function fireballz() {
-    function firework() {
-        const there = magik.aspecto();
-        magik.stella(there);
-    }
-    magik.doNTimes(fireball, 5);
-}
