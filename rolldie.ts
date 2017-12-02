e0baf0d468a280598590d5b2f65a947930cd891e
jasonlock123 <jason@locksplace.com>
2017-12-02 11:46:40 +0000

    Update rolldie.ts

--- rolldie.ts
+++ rolldie.ts
@@ -1,5 +1,5 @@
 magik = magikcraft.io;
-function rollDie(sides){
+function rolldie(sides){
     const dieroll = magik.random(1, sides);
     magik.dixit(`You rolled 1d${sides} and got: ${dieroll}`);
     return dieroll;
